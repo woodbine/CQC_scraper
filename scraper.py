d57478e52dfed05f8dcdefaa2271591286d95c35
woodbine <ian@spendnetwork.com>
2015-05-05 20:18:38 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -18,15 +18,12 @@ df = pandas.read_csv(csvUrl, skiprows=4)
 iterator = df.ix[:, 12].iteritems() # gets the column of urls in the csv file and then iterate
 
 for itr in iterator:
-  print itr[0]
-  '''
-  url = itr.str
+  url = itr[1]
   print url
   idhtml = urllib2.urlopen(itr)
   idsoup = BeautifulSoup(idhtml)
   idname = idsoup.find('span',{'class':'facility-name'}).text
   print idname
-  '''
-todays_date = str(datetime.now())
+  todays_date = str(datetime.now())
 
 #scraperwiki.sqlite.save(unique_keys=['l'], data={"l": fileurl, "f": filename, "d": todays_date })
